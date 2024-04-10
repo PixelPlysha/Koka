4dac1366f8a9313a9fdf141b703a42d57031137c
PixelPlysha <ibif3485@gmail.com>
2024-04-10 20:01:20 -0300

    getStarted section

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+import '../assets/scss/contact.scss';
+
+import { useTheme } from '../js/components/theme.js';
+import { useBurger } from '../js/components/burger.js';
+import { usePhone } from '../js/components/contact/phone.js';
+
+
+
+useTheme();
+useBurger();
+usePhone();
