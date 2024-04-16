4d82b3ba3cee04e5a537d5ac3d7b8525aedfdbc0
PixelPlysha <ibif3485@gmail.com>
2024-04-15 22:53:11 -0300

    Added Blog Page

---
+++ src/js/activity.js
@@ -0,0 +1,8 @@
+import '/scss/activity.scss';
+
+import { useTheme } from '../js/components/theme.js';
+import { useBurger } from '../js/components/burger.js';
+
+
+useTheme();
+useBurger();
---
+++ src/js/blog.js
@@ -0,0 +1,8 @@
+import '/scss/blog.scss';
+
+import { useTheme } from '../js/components/theme.js';
+import { useBurger } from '../js/components/burger.js';
+
+
+useTheme();
+useBurger();
