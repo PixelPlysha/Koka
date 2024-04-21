0defb0f4aff83add317e9d9cedab5e4de43c20da
PixelPlysha <ibif3485@gmail.com>
2024-04-21 11:03:47 -0300

    deploy-fixed-vite-config

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,8 @@ export default defineConfig({
         pricing: './src/pricing.html',
         contact: './src/contact.html',
         about: './src/about.html',
+        blog: './src/blog.html',
+        activity: './src/activity.html',
       },
     },
   },
