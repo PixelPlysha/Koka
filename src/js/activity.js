4835f7b1b2dce6babea83124ebd831919f4ec66f
PixelPlysha <ibif3485@gmail.com>
2024-04-20 23:40:01 -0300

    activity section

--- src/js/activity.js
+++ src/js/activity.js
@@ -2,7 +2,11 @@ import '/scss/activity.scss';
 
 import { useTheme } from '../js/components/theme.js';
 import { useBurger } from '../js/components/burger.js';
+import { useUpdatesSlider } from './components/home/slider.js';
+import { useArticlesSlider } from './components/home/slider.js';
 
 
 useTheme();
 useBurger();
+useUpdatesSlider();
+useArticlesSlider();
--- src/js/components/home/slider.js
+++ src/js/components/home/slider.js
@@ -60,3 +60,33 @@ export const useAttentionSlider = () => {
       },
     });
   };
+
+
+  export const useUpdatesSlider = () => {
+    new Swiper('.updates__slider', {
+      slidesPerView: 'auto',
+      spaceBetween: 32,
+      loop: true,
+      centeredSlides: true,
+      breakpoints: {
+        992: {
+          centeredSlides: false,
+        },
+      },
+    });
+  };
+
+
+
+  export const useArticlesSlider = () => {
+    new Swiper('.articles__slider', {
+      modules: [Navigation],
+      slidesPerView: 'auto',
+      spaceBetween: 22,
+      loop: true,
+      navigation: {
+        prevEl: '.articles__btn--prev',
+        nextEl: '.articles__btn--next',
+      },
+    });
+  };
